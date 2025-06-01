4962790fd5c9f5ee3cdcd78fee6ec1c1742bc5db
PatientRed <154473346+PatientRed@users.noreply.github.com>
2025-06-02 02:10:29 +0300

    Action factory dummy draft added

---
+++ Core/Action/BaseActionFactory.cs
@@ -0,0 +1,24 @@
+﻿using Sitta.Core.TypeSystem;
+
+namespace Sitta.Core.Action
+{
+    public static class BaseActionFactory
+    {
+        public static IAction Create(IActionDescription actiondescription)
+        {
+            if (actiondescription == null)
+                throw new ArgumentNullException(nameof(actiondescription));
+
+            //TODO: that is very naive and seems not optimal
+            switch (actiondescription.SystemActionType)
+            {
+                case var t when t.IsAssignableFrom(typeof(ModifyTargetAction)):
+                    return new ModifyTargetAction(new propertyGenerator.Produce(actiondescription));
+                //TODO: or?
+                //return new ModifyTargetAction(this.ProduceProps(actiondescription));
+                default:
+                    throw new ArgumentException(nameof(actiondescription.SystemActionType));
+            }
+        }
+    }
+}
