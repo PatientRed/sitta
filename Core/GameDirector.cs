fd48cf927e81f31b7f9e9a280ef4b2239be18d6f
PatientRed <154473346+PatientRed@users.noreply.github.com>
2025-06-23 23:08:43 +0300

    Couple of draft classes created

---
+++ Core/GameDirector.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sitta.Core
+{
+    internal class GameDirector
+    {
+        public void InitializeTypes()
+        {
+
+        }
+    }
+}
---
+++ Core/GameLoader.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sitta.Core
+{
+    internal class GameLoader
+    {
+        public void Load()
+        {
+        }
+    }
+}
