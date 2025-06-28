6114854016e5da1e64b9f5c920ad8a77b163b27b
PatientRed <154473346+PatientRed@users.noreply.github.com>
2025-06-29 01:13:13 +0300

    Parser stub added

---
+++ Core/Parser.cs
@@ -0,0 +1,28 @@
+﻿using Sitta.Core.Descriptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sitta.Core
+{
+    public interface IParser
+    {
+        public IDescription Parse(string description);
+        public IEnumerable<IDescription> Parse(IEnumerable<string> description);
+    }
+
+    internal class Parser : IParser
+    {
+        public IDescription Parse(string description)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<IDescription> Parse(IEnumerable<string> description)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
