26a93fdf831bf97eebe8da49abcf11b07d5ab129
PatientRed <154473346+PatientRed@users.noreply.github.com>
2025-06-29 01:09:25 +0300

    IDescription extended

--- Core/Descriptions/IDescription.cs
+++ Core/Descriptions/IDescription.cs
@@ -5,22 +5,31 @@
         public string UserType { get; }
     }
 
+    public interface ITypeDescription : IDescription
+    {
+        public string Name { get; }
+    }
+
     //TODO: replace system type fields type with respective enums?
     //No reflection supposed yet, so no real necessity in having Type variable.
     //Moreover types can't be switched naturally (switches on type variable are clumsy).
-    public interface IParamDescription : IDescription
+    public interface IParamDescription : ITypeDescription
     {
-        public string Name { get; }
         public Type SystemParamType { get; }
     }
 
-    public interface IActionDescription : IDescription
+    public interface IActionDescription : ITypeDescription
     {
         public IEnumerable<IParamDescription> Parameters { get; }
         public Type SystemActionType { get; }
     }
 
-    public interface IComponentDescription : IDescription
+    public interface IComponentDescription : ITypeDescription
+    {
+
+    }
+
+    public interface IObjectDescription : IDescription
     {
 
     }
