407f152704f5cf4aff2e2989b713485c5af90e49
PatientRed <154473346+PatientRed@users.noreply.github.com>
2025-06-29 01:08:19 +0300

    GameLoader stub reworked to use IDataSource

--- Core/GameLoader.cs
+++ Core/GameLoader.cs
@@ -1,15 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Sitta.Core.Descriptions;
 
 namespace Sitta.Core
 {
-    internal class GameLoader
+    public interface ILoader
     {
-        public void Load()
+        (IEnumerable<ITypeDescription> types, IEnumerable<IObjectDescription> objects) Load();
+    }
+
+    internal class GameLoader : ILoader
+    {
+        private readonly IDataSource _dataSource;
+
+        public (IEnumerable<ITypeDescription> types, IEnumerable<IObjectDescription> objects) Load()
         {
+            var content = _dataSource.Load();
+            var result = LoadParser().Parse(content);
+
+            throw new NotImplementedException();
+
+            return Process(result);
+        }
+
+        private static (IEnumerable<ITypeDescription> types, IEnumerable<IObjectDescription> objects) Process(IEnumerable<IDescription> content)
+        {
+            throw new NotImplementedException();
+        }
+
+        private IParser LoadParser()
+        {
+            throw new NotImplementedException();
+        }
+
+        public GameLoader(IDataSource dataSource)
+        {
+            _dataSource = dataSource;
         }
     }
 }
