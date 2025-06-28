6efec48a42ad19100f29f0416c95231ed2502202
PatientRed <154473346+PatientRed@users.noreply.github.com>
2025-06-29 01:07:40 +0300

    IDataSource added

---
+++ Core/DataSource.cs
@@ -0,0 +1,15 @@
+﻿namespace Sitta.Core
+{
+    public interface IDataSource
+    {
+        public IEnumerable<string> Load();
+    }
+
+    public abstract class DataSource : IDataSource
+    {
+        public IEnumerable<string> Load()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
